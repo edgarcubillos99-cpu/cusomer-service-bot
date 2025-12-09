9d27e49abe08c5e80dae8ce81d98c517791dcf3c
myselfcarvajal <myselfcarvajal@gmail.com>
2025-12-09 14:25:51 -0500

    feat: integrate Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //CONFIGURANDO PIPES GLOBALES
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //HABILITANDO LOS CORS
+  app.enableCors();
+
+  //SI EL ENTORNO ES DE DESARROLLO
+  if (process.env.ENTORNO === 'DEV') {
+    const config = new DocumentBuilder()
+      .setTitle('Customer Service Bot Documentation')
+      .setDescription('Customer Service Bot api docs')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
